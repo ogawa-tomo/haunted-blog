4e93221753cea9df546bbcc3a6b4d176492bdfae
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-17 22:21:48 +0900

    sanitizeメソッドをActiveSupport::SafeBuffer.newで置き換える

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    sanitize(blog.content.split("\n").map { |line| h(line) }.join('<br>'))
+    ActiveSupport::SafeBuffer.new(blog.content.split("\n").map { |line| h(line) }.join('<br>'))
   end
 end
