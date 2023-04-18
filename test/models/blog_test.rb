806704454f2ac2cba4224b0bd13cc5510a1657ca
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-18 21:39:34 +0900

    blogモデルのテストをvalid?を用いて書き換え

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -6,6 +6,6 @@ class BlogTest < ActiveSupport::TestCase
   test '通常ユーザーはアイキャッチを設定できない' do
     blog = blogs(:bob_blog)
     blog.random_eyecatch = true
-    assert_not blog.save
+    assert_not blog.valid?
   end
 end
